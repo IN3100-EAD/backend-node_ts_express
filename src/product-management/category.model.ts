c53f37fa4584c47246c8e3e1cffd90bd321f77e0
Malitha Sandaruwan <sandaruwanmalitha2018@gmail.com>
2023-11-07 09:07:51 +0530

    Add category and product models.

---
+++ src/product-management/category.model.ts
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+interface CategoryAttr {
+  name: string;
+  description: string;
+}
+
+interface CategoryDoc
+  extends mongoose.Document,
+    CategoryAttr {}
+
+const CategorySchema: mongoose.Schema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Category name is required"],
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const CategoryModel = mongoose.model<CategoryDoc>(
+  "Category",
+  CategorySchema
+);
+
+export default CategoryModel;
+export { CategoryDoc };
